8f50083bed8be6366b6f24ac5bfe5cd79a859eea
Charlie Kelson <charlie.kelson@gettyimages.com>
2020-10-19 12:31:59 -0700

    Add functionality for all tests to pass following README.md

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import traceback
+
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -41,44 +43,111 @@ To submit your homework:
 
 """
 
+def home():
+    page = """
+    <h1>Welcome to the WSGI Calculator</h1>
+    <h3>Here are the operations you can do:</h3>
+    <table>
+        <tr><th>Add</th><td>/add/{num1}/{num2}</td></tr>
+        <tr><th>Subtract</th><td>/subtract/{num1}/{num2}</td></tr>
+        <tr><th>Multiply</th><td>/multiply/{num1}/{num2}</td></tr>
+        <tr><th>Divide</th><td>/divide/{num1}/{num2}</td></tr>
+    </table>
+    """
+    return page
 
-def add(*args):
+
+def add(num1, num2):
     """ Returns a STRING with the sum of the arguments """
+    adding = int(num1) + int(num2)
+    page = f"""
+            <h1> You have added {num1} and {num2} which equals {adding}</h1>
+            """
+    return page
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
 
-    return sum
+def subtract(num1, num2):
+    """ Returns a STRING with the difference of the arguments """
+    difference = int(num1) - int(num2)
+    page = f"""
+            <h1> You have subtracted {num1} and {num2} which equals {difference}</h1>
+            """
+    return page
 
-# TODO: Add functions for handling more arithmetic operations.
+
+def multiply(num1, num2):
+    """ Returns a STRING of the two numbers multiplied"""
+    times = int(num1) * int(num2)
+    page = f"""
+            <h1> You have multiplied {num1} and {num2} which equals
+            {times}</h1>
+            """
+    return page
+
+
+def divide(num1, num2):
+    """ Returns a STRING of the two numbers divided"""
+    divided = int(num1) / int(num2)
+    page = f"""
+            <h1> You have divided {num1} and {num2} which equals
+            {divided}</h1>
+            """
+    return page
+
 
 def resolve_path(path):
+
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
+    funcs = {
+        '': home,
+        'add': add,
+        'multiply': multiply,
+        'divide': divide,
+        'subtract': subtract,
+    }
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
 
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
+
